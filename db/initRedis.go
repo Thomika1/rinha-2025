4d9ef790cf4d81d87617560c07dc8aca289116b6
Thomika1 <thomazmbonfim@gmail.com>
2025-07-28 12:31:54 -0300

    Feature: Added db initialization

--- db/initRedis.go
+++ db/initRedis.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"log"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -10,11 +11,18 @@ var Client *redis.Client
 var RedisCtx = context.Background()
 
 func InitRedis() {
-	Client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+	Client = redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
 		Password: "", // No password set
 		DB:       0,  // Use default DB
 		Protocol: 2,  // Connection protocol
 	})
 
+	_, err := Client.Ping(RedisCtx).Result()
+	if err != nil {
+		// Use log.Fatalf para encerrar a aplicação se a conexão falhar.
+		log.Fatalf("Não foi possível conectar ao Redis: %v", err)
+	}
+
+	log.Println("Conectado ao Redis com sucesso!")
 }
